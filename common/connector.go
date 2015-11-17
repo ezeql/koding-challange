55640c90e11fb5966e4efeb9ee88421579c34b76
Ezequiel Moreno <ezeqle@gmail.com>
2015-11-17 20:33:13 -0300

    improve connector

--- common/connector.go
+++ common/connector.go
@@ -3,70 +3,80 @@ package common
 import (
 	"fmt"
 	"github.com/streadway/amqp"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 type RabbitMQConnector struct {
-	Ch   *amqp.Channel
-	Q    *amqp.Queue
-	Conn *amqp.Connection
+	conn    *amqp.Connection
+	channel *amqp.Channel
+	tag     string
+	done    chan error
 }
 
 func BuildRabbitMQConnector(host string, port int, user string, password string) (*RabbitMQConnector, error) {
 	var err error
 	mq := &RabbitMQConnector{}
+	Info("connecting to RabbitMQ")
+
 	url := fmt.Sprintf("amqp://%s:%s@%s:%v", user, password, host, port)
 
-	if mq.Conn, err = amqp.Dial(url); err != nil {
+	if mq.conn, err = amqp.Dial(url); err != nil {
 		return nil, err
 	}
-	if mq.Ch, err = mq.Conn.Channel(); err != nil {
+	if mq.channel, err = mq.conn.Channel(); err != nil {
 		return nil, err
 	}
-	if err = mq.Ch.ExchangeDeclare("logs", "fanout", true, false, false, false, nil); err != nil {
+	if err = mq.channel.ExchangeDeclare("logs", "fanout", true, false, false, false, nil); err != nil {
 		return nil, err
 	}
-	if err = mq.Ch.Qos(1, 0, false); err != nil {
+	if err = mq.channel.Qos(1, 0, false); err != nil {
 		return nil, err
 	}
-	q, err := mq.Ch.QueueDeclare("distinctName", true, false, false, false, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	mq.Q = &q
 	return mq, nil
 }
 
-func (m *RabbitMQConnector) Handle(f func([]byte)) {
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+func (m *RabbitMQConnector) Handle(tag string, f func([]byte)) error {
 
+	q, err := m.channel.QueueDeclare(tag, true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	if err = m.channel.QueueBind(q.Name, "", "logs", false, nil); err != nil {
+		return err
+	}
+
+	msgs, err := m.channel.Consume(tag, tag, false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
 	go func() {
-		defer m.close()
-		msgs, err := m.Ch.Consume(m.Q.Name, "", false, false, false, false, nil)
-		if err != nil {
-			panic(err)
-		}
-		for d := range msgs {
-			f(d.Body)
-			if err := d.Ack(false); err != nil {
-				panic(err)
-			}
-		}
+		Info("closing: %s", <-m.conn.NotifyClose(make(chan *amqp.Error)))
+		//TODO: try to recconect
 	}()
+	m.tag = tag
 
-	<-signalChan
+	go handle(msgs, f, m.done)
+	return nil
 }
 
-func (m *RabbitMQConnector) close() {
-	if err := m.Ch.Close(); err != nil {
-		log.Println("error clossing ampq channel:", err)
+func handle(deliveries <-chan amqp.Delivery, f func([]byte), done chan error) {
+	for d := range deliveries {
+		d.Ack(false)
+		f(d.Body)
 	}
-	if err := m.Conn.Close(); err != nil {
-		log.Println("error clossing ampq connection:", err)
+	Info("handle: deliveries channel closed")
+	done <- nil
+}
+
+func (c *RabbitMQConnector) Close() error {
+	// will close() the deliveries channel
+	if err := c.channel.Cancel(c.tag, true); err != nil {
+		return fmt.Errorf("Consumer cancel failed: %s", err)
 	}
+	if err := c.conn.Close(); err != nil {
+		return fmt.Errorf("AMQP connection close error: %s", err)
+	}
+	defer Info("AMQP shutdown OK")
+	// wait for handle() to exit
+	return <-c.done
 }
