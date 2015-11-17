1755bbc48e054b2ed5022fae400cea721dbd015d
Ezequiel Moreno <ezeqle@gmail.com>
2015-11-17 20:46:37 -0300

    improve hourly log worker

--- workers/hourlylog/main.go
+++ workers/hourlylog/main.go
@@ -10,47 +10,69 @@ import (
 )
 
 var (
-	rabbitHost     = flag.String("rabbit-host", "localhost", "RabbitMQ host")
+	rabbitHost     = flag.String("rabbit-host", "127.0.0.1", "RabbitMQ host")
 	rabbitPort     = flag.Int("rabbit-port", 5672, "RabbitMQ port")
 	rabbitUser     = flag.String("rabbit-user", "guest", "RabbitMQ username")
 	rabbitPassword = flag.String("rabbit-password", "guest", "RabbitMQ password")
-	mongoDBHost    = flag.String("mongodb-host", "localhost", "MongoDB host")
+	mongoDBHost    = flag.String("mongodb-host", "127.0.0.1", "MongoDB host")
 	mongoDBPort    = flag.Int("mongodb-port", 27017, "MongoDB port")
 )
+
+type Mongo struct {
+	*mgo.Session
+}
 
 func main() {
 	flag.Parse()
 
-	logs, err := common.BuildRabbitMQConnector(*rabbitHost, *rabbitPort, *rabbitUser, *rabbitPassword)
+	connector, err := common.BuildRabbitMQConnector(*rabbitHost, *rabbitPort, *rabbitUser, *rabbitPassword)
 	if err != nil {
 		log.Fatalln("Error on RabbitMQ:", err)
 	}
+	defer connector.Close()
 
-	session, err := mgo.Dial(fmt.Sprintf("%s:%v", *mongoDBHost, *mongoDBPort))
+	session, err := OpenMongo(*mongoDBHost, *mongoDBPort)
 	if err != nil {
-		log.Fatalln("error dialing MongoDB", err)
+		log.Fatalln("Error on Mongo:", err)
 	}
 	defer session.Close()
 
 	c := session.DB("koding").C("entries")
 
-	logs.Handle(func(b []byte) {
-		d, err := common.FromJSONBytes(b)
-		if err != nil {
-			panic(err)
-		}
-		if err = c.Insert(d); err != nil {
-			log.Fatalln("cannot insert metric in mongo", err)
-		}
-		index := mgo.Index{
-			Key:         []string{"time"},
-			Name:        "time_ttl",
-			ExpireAfter: time.Hour,
-		}
-		//delete if exists..
-		c.DropIndex(index.Name)
-		if err = c.EnsureIndex(index); err != nil {
-			log.Fatalln("cannot create index on mongodb", err)
+	err = connector.Handle("hourly-log", func(b []byte) {
+		d := common.MustUnmarshallFromJSON(b)
+		if err = InsertMetric(d); err != nil {
+			log.Println("error inserting in mongo", err)
 		}
 	})
+
+	if err != nil {
+		log.Fatalln("error connecting to Rabbit")
+	}
+
+	select {}
+}
+
+func OpenMongo(host string, port int) (*Mongo, error) {
+	session, err := mgo.Dial(fmt.Sprintf("%s:%v", host, port))
+	if err != nil {
+		return nil, err
+	}
+	m := &Mongo{session}
+	return m, nil
+}
+
+func (m *Mongo) InsertMetric(d common.MetricData) error {
+	c := m.DB("koding").C("entries")
+	if err := c.Insert(d); err != nil {
+		return err
+	}
+	index := mgo.Index{
+		Key:         []string{"time"},
+		Name:        "time_ttl",
+		ExpireAfter: time.Hour,
+	}
+	//delete if exists..
+	c.DropIndex(index.Name)
+	return c.EnsureIndex(index)
 }
