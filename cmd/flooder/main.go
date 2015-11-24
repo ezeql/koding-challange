2dfcac1aee856d96dd2c555059cf77f6616c7d0c
Ezequiel Moreno <ezeqle@gmail.com>
2015-11-24 14:20:00 -0300

    add url flag to flooder

--- cmd/flooder/main.go
+++ cmd/flooder/main.go
@@ -3,28 +3,38 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+)
+
+var (
+	url = flag.String("url", "http://127.0.0.1:8080", "metric endpoint url")
 )
 
 func main() {
 	log.Println("Flooding with metrics... =D")
 
 	client := &http.Client{}
-	for range time.Tick(time.Millisecond) {
-		m := map[string]interface{}{
-			"username": "kodingbot",
-			"count":    1,
-			"metric":   "kite_call",
-		}
+	dest := fmt.Sprintf("%s/metric", *url)
+
+	m := map[string]interface{}{
+		"username": "kodingbot",
+		"count":    1,
+		"metric":   "kite_call",
+	}
+
+	for range time.Tick(500 * time.Millisecond) {
+
 		json, _ := json.Marshal(m)
 
 		contentReader := bytes.NewReader(json)
 
-		req, _ := http.NewRequest("POST", "http://127.0.0.1:8080/metric", contentReader)
+		req, _ := http.NewRequest("POST", dest, contentReader)
 		req.Header.Set("Content-Type", "application/json")
-		_, err = client.Do(req)
+		_, err := client.Do(req)
 		if err != nil {
 			log.Fatalln(err)
 		}
